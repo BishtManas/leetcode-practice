f77878c142bb43ca8df15dbebdd8103940e868a0
manas <bishtmanas2008@gmail.com>
2025-07-04 16:56:35 +0530

    to lower case (question type : easy)

---
+++ 709 To Lower case /index.py
@@ -0,0 +1,7 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        word = s.lower()
+        return word
+info = Solution()
+print(info.toLowerCase("Manas"))# output : manas
+print(info.toLowerCase("MANAS"))# output : manas
