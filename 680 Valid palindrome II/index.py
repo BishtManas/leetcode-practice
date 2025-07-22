53117827e1f5460cafd03c341954dcb9b8032284
Manas Bisht <bishtmanas2008@gmail.com>
2025-07-22 11:30:48 +0530

    valid palindrome II (question type : easy)

---
+++ 680 Valid palindrome II/index.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_pali_range(left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                # Try skipping left or right character
+                return is_pali_range(left + 1, right) or is_pali_range(left, right - 1)
+            left += 1
+            right -= 1
+        return True
+info = Solution()
+print(info.validPalindrome("aba"))# output : true
+print(info.validPalindrome("abc"))# output : false
+print(info.validPalindrome("abcba"))# output : true
