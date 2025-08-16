998cec418955e7cf01a5f685f5bed8929b7ff40d
Manas Bisht <bishtmanas2008@gmail.com>
2025-08-16 23:40:46 +0530

    arranging coins ( question type : easy)

--- 441 Arranging Coins/index.py
+++ 441 Arranging Coins/index.py
@@ -1,31 +1,11 @@
 import math
 
-def arrange_coins_iterative(n):
-    rows = 0
-    while n >= rows + 1:
-        rows += 1
-        n -= rows
-    return rows
+def arrange_coins(n):
+    return int((math.sqrt(1 + 8 * n) - 1) // 2)
 
-def arrange_coins_math(n):
-    return int((math.sqrt(1 + 8*n) - 1) // 2)
+# LeetCode Examples
+examples = [5, 8]
 
-def arrange_coins_binary(n):
-    left, right = 0, n
-    while left <= right:
-        mid = (left + right) // 2
-        curr = mid * (mid + 1) // 2
-        if curr == n:
-            return mid
-        if curr < n:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return right
-
-# Input from user
-if __name__ == "__main__":
-    n = int(input("Enter number of coins: "))
-    print("Iterative Method:", arrange_coins_iterative(n))
-    print("Math Method:", arrange_coins_math(n))
-    print("Binary Search Method:", arrange_coins_binary(n))
+for n in examples:
+    print(f"Input: n = {n}")
+    print(f"Output: {arrange_coins(n)}\n")
