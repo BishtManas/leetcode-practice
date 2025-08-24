7d89df5b0c4f4e2a23cafe68ac04305eaa610f0a
Manas Bisht <bishtmanas2008@gmail.com>
2025-08-24 18:53:25 +0530

    longest subarray of 1's after deleting one element ( question type : medium)

---
+++ 1493 Longest Subarray of 1's After Deleting One Element/index.py
@@ -0,0 +1,20 @@
+def longest_subarray(nums):
+    left = 0
+    zero_count = 0
+    max_length = 0
+
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            zero_count += 1
+        
+        while zero_count > 1:
+            if nums[left] == 0:
+                zero_count -= 1
+            left += 1
+        
+        max_length = max(max_length, right - left)
+    
+    return max_length
+
+nums = [0,1,1,1,0,1,1,0,1]
+print(longest_subarray(nums)) 
