9395028375e249577317e8c9b62b54e7819893d9
Manas Bisht <bishtmanas2008@gmail.com>
2025-10-09 16:48:19 +0530

    create a new folder and move.

---
+++ 611 Valid Triangle Number/index.py
@@ -0,0 +1,24 @@
+class Solution:
+    def triangleNumber(self, nums):
+        nums.sort()
+        n = len(nums)
+        count = 0
+
+        for k in range(n - 1, 1, -1):  # Fix the largest side
+            i, j = 0, k - 1
+            while i < j:
+                if nums[i] + nums[j] > nums[k]:
+                    count += j - i
+                    j -= 1
+                else:
+                    i += 1
+        return count
+
+# Example test cases
+if __name__ == "__main__":
+    nums1 = [2, 2, 3, 4]
+    nums2 = [4, 2, 3, 4]
+    
+    sol = Solution()
+    print("Input:", nums1, "-> Output:", sol.triangleNumber(nums1))  # Expected 3
+    print("Input:", nums2, "-> Output:", sol.triangleNumber(nums2))  # Expected 4
