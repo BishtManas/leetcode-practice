6f5fe175b1be5418200b38d152a90100a36a084e
BishtManas <bishtmanas2008@gmail.com>
2025-11-02 22:32:54 +0530

    decode ways (question type : medium)

---
+++ 91 Decode Ways/index.py
@@ -0,0 +1,24 @@
+def numDecodings(s):
+    if not s or s[0] == "0":
+        return 0
+
+    n = len(s)
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        one_digit = int(s[i - 1:i])
+        two_digits = int(s[i - 2:i])
+
+        if 1 <= one_digit <= 9:
+            dp[i] += dp[i - 1]
+        if 10 <= two_digits <= 26:
+            dp[i] += dp[i - 2]
+
+    return dp[n]
+
+
+# 👇 Test it here
+s = "226"
+print(numDecodings(s))  # Output: 3
