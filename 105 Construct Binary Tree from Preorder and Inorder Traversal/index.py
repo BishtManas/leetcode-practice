091b5c19a9c5e2ead9762c9b2fe1fa308e64525d
BishtManas <bishtmanas2008@gmail.com>
2025-11-12 23:43:20 +0530

    Construct Binary Tree from Preorder and Inorder Traversal (question type : medium)

---
+++ 105 Construct Binary Tree from Preorder and Inorder Traversal/index.py
@@ -0,0 +1,55 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        if not preorder or not inorder:
+            return None
+
+        root_val = preorder[0]
+        root = TreeNode(root_val)
+
+        mid = inorder.index(root_val)
+
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        return root
+
+
+# Helper function to print tree in level order (for testing)
+from collections import deque
+def printLevelOrder(root):
+    if not root:
+        print("Empty Tree")
+        return
+
+    queue = deque([root])
+    result = []
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+
+    # Remove trailing None values
+    while result and result[-1] is None:
+        result.pop()
+
+    print(result)
+
+
+# Example Test
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+
+sol = Solution()
+tree = sol.buildTree(preorder, inorder)
+printLevelOrder(tree)
