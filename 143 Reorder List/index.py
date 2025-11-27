19fddbad6f35587dea79ad8167134f49efb9c08c
BishtManas <bishtmanas2008@gmail.com>
2025-11-27 21:41:53 +0530

    reorder list (question type : medium)'

---
+++ 143 Reorder List/index.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head):
+        if not head or not head.next:
+            return head
+
+        # 1. Find middle (slow/fast pointers)
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # 2. Reverse second half
+        prev = None
+        curr = slow
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        # 3. Merge two halves
+        first, second = head, prev
+        while second.next:
+            tmp1 = first.next
+            tmp2 = second.next
+
+            first.next = second
+            second.next = tmp1
+
+            first = tmp1
+            second = tmp2
