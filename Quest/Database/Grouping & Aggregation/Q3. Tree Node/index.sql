5b52faf4bd16137957c9e5df62c508aa70259c9b
BishtManas <bishtmanas2008@gmail.com>
2025-12-16 22:48:02 +0530

    code for quest question (grouping and aggregation)

---
+++ Quest/Database/Grouping & Aggregation/Q3. Tree Node/index.sql
@@ -0,0 +1,8 @@
+SELECT 
+    t.id,
+    CASE
+        WHEN t.p_id IS NULL THEN 'Root'
+        WHEN t.id IN (SELECT DISTINCT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM Tree t;
