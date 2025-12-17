3d2f31e74c068140640f3f3f230f9e6e59518046
BishtManas <bishtmanas2008@gmail.com>
2025-12-17 13:15:24 +0530

    code for quest (SQL II )

---
+++ Quest/Database/SQL II/Q4. Human Traffic of Stadium/index.sql
@@ -0,0 +1,17 @@
+SELECT
+    id,
+    visit_date,
+    people
+FROM Stadium
+WHERE people >= 100
+AND (
+    (id - 1 IN (SELECT id FROM Stadium WHERE people >= 100)
+     AND id - 2 IN (SELECT id FROM Stadium WHERE people >= 100))
+ OR
+    (id - 1 IN (SELECT id FROM Stadium WHERE people >= 100)
+     AND id + 1 IN (SELECT id FROM Stadium WHERE people >= 100))
+ OR
+    (id + 1 IN (SELECT id FROM Stadium WHERE people >= 100)
+     AND id + 2 IN (SELECT id FROM Stadium WHERE people >= 100))
+)
+ORDER BY visit_date;
