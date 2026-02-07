25f8d0574de60bff4d6e490e2b4a75701ec42468
BishtManas <bishtmanas2008@gmail.com>
2026-02-07 11:21:13 +0530

    Minimum Deletions to Make String Balanced ( question type : medium)

---
+++ 1653 Minimum Deletions to Make String Balanced/index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        res = len(s)
+        a = 0
+        b = 0
+
+        for c in s:
+            a += (c == 'a')
+
+        for c in s:
+            a -= (c == 'a')
+            res = min(res, a + b)
+            b += (c == 'b')
+
+        return res
+if __name__ == "__main__":
+    sol = Solution()
+    print(f'Answer is : {sol.minimumDeletions(s = "aababbab")}')
