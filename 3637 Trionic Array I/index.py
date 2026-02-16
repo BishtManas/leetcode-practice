3ebd50f62dcd8c9afd4bc2352a1b7ae423f6a8ab
BishtManas <bishtmanas2008@gmail.com>
2026-02-16 23:05:29 +0530

    Trionic Array I (question type : easy)

---
+++ 3637 Trionic Array I/index.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def isTrionic(self, a: List[int]) -> bool:
+        return [*islice((q for q,_ in 
+            groupby((v<u)-(v>u) for v,u in pairwise(a))),4)]==[1,-1,1]
