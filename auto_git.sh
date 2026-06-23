1d5f6838e270d8a2d6330b7274194198eaf65d58
BishtManas <bishtmanas2008@gmail.com>
2026-06-23 18:36:03 +0530

    commit a msg

---
+++ auto_git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+start_date="2025-01-01"
+end_date="2025-06-28"
+
+current_date="$start_date"
+
+while [ "$(date -d "$current_date" +%s)" -le "$(date -d "$end_date" +%s)" ]
+do
+    for i in {1..9}
+    do
+        echo "- Update on $current_date commit $i" >> README.md
+
+        git add README.md
+
+        hour=$((9 + i))
+
+        GIT_AUTHOR_DATE="$current_date $hour:00:00" \
+        GIT_COMMITTER_DATE="$current_date $hour:00:00" \
+        git commit -m "README update $i - $current_date"
+    done
+
+    current_date=$(date -I -d "$current_date + 1 day")
+done
